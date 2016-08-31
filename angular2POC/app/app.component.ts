390274b0f1e211bc7974306d45fdb9796b085885
unknown <nishant.awasthi1@DEL1-LHP-40723.synapse.com>
2016-08-31 14:48:15 +0530

    First commit

---
+++ angular2POC/app/app.component.ts
@@ -0,0 +1,9 @@
+import  { Component } from 'angular2/core';
+
+@Component({
+    selector: 'pm-app',
+    template:'<div><h1>{{pageTitle}} </h1></div>'
+})
+export class AppComponent{
+    pageTitle: string = 'Electricity Billing Management';
+}
